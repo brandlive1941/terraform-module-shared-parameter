586483011635cf9f2f30b3d8f26fb0e2ac605b31
drewmercer-bl <drew.mercer@brandlive.com>
2025-08-04 11:36:16 -0700

    work?

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "ssm_parameter_share_arn" {
   description = "The ARN of the created SSM parameter share."
-  value       = keys(var.ssm_paths_to_share)
+  value       = var.ssm_paths_to_share
 }
 
